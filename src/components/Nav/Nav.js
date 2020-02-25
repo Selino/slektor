139746483591f26733b99b18429258cade60c662
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-02-25 13:48:01 -0800

    Adding app logo, name, and boostrap navbar component to the nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,21 @@
 import React from "react"
+import { Navbar } from "react-bootstrap"
+import logo from "../../logo.svg"
 
 export default function Nav() {
-  return <div>Nav</div>
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Navbar.Brand href='#home'>
+        <img
+          className='App-logo'
+          alt='logo'
+          src={logo}
+          width='30'
+          height='30'
+          className='d-inline-block align-top'
+        />{" "}
+        Slektor App
+      </Navbar.Brand>
+    </Navbar>
+  )
 }
