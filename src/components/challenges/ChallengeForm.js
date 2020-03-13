e38c5221ed06cf903b9f75b2efa700158b06c40b
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-03-12 20:20:53 -0700

    Adding working calendar

--- src/components/challenges/ChallengeForm.js
+++ src/components/challenges/ChallengeForm.js
@@ -7,19 +7,25 @@ import {
   FormControl
 } from "react-bootstrap"
 import { v4 as uuidv4 } from "uuid"
+import moment from "moment"
+import "react-dates/initialize"
+import { SingleDatePicker } from "react-dates"
+import "react-dates/lib/css/_datepicker.css"
 
 function ChallengeForm() {
   const [state, setState] = useState({
     bookTitle: "Book Title Gets Passed Into Here Book",
     amount: 5,
-    bookId: uuidv4()
+    bookId: uuidv4(),
+    calendarFocused: false
   })
   const [readers] = useState(["Tomy", "Dick", "Jane"])
+  const [myDate, setMyDate] = useState(moment())
 
   const onAmountChange = e => {
     const amount = e.target.value
     if (amount.match(/^\d*?$/) && amount <= 100 && amount > 0) {
-      setState({ ...state, amount })
+      setState({ ...state, amount: parseInt(amount, 10) })
     }
   }
 
@@ -31,45 +37,67 @@ function ChallengeForm() {
     )
   })
 
+  const onDateChange = date => {
+    setMyDate(date)
+  }
+
+  const onFocusChange = ({ focused }) => {
+    setState(() => ({ ...state, calendarFocused: focused }))
+  }
+
   const handleSubmit = () => {
     console.log(state)
   }
 
   return (
-    <Form>
-      <Form.Group controlId='formBasicTitle'>
-        <Form.Control
-          onChange={e => (state.bookTitle = e.target.value)}
-          defaultValue={state.bookTitle}
-          type='text'
-          placeholder={state.bookTitle}
-          disabled
-        />
-      </Form.Group>
+    <div>
+      <Form>
+        <Form.Group controlId='formBasicTitle'>
+          <Form.Control
+            onChange={e => (state.bookTitle = e.target.value)}
+            defaultValue={state.bookTitle}
+            type='text'
+            placeholder={state.bookTitle}
+            disabled
+          />
+        </Form.Group>
+        <InputGroup className='mb-3'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>$</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            type='number'
+            onChange={onAmountChange}
+            aria-label='Amount (to the nearest dollar)'
+            //   placeholder={state.amount}
+            value={state.amount}
+            autoFocus
+          />
+          <InputGroup.Append>
+            <InputGroup.Text>$100 max</InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <Form.Group controlId='exampleForm.ControlSelect1'>
+          <FormLabel>Select reader</FormLabel>
+          <Form.Control as='select'>{readersSelectItems}</Form.Control>
+        </Form.Group>
 
-      <InputGroup className='mb-3'>
-        <InputGroup.Prepend>
-          <InputGroup.Text>$</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl
-          type='number'
-          onChange={onAmountChange}
-          aria-label='Amount (to the nearest dollar)'
-          //   placeholder={state.amount}
-          value={state.amount}
-          autoFocus
-        />
-        <InputGroup.Append>
-          <InputGroup.Text>$100 max</InputGroup.Text>
-        </InputGroup.Append>
-      </InputGroup>
+        <Form.Group controlId='exampleForm.ControlSelect1'>
+          <FormLabel>Select end date</FormLabel>
+          <SingleDatePicker
+            date={myDate}
+            onDateChange={onDateChange}
+            focused={state.calendarFocused}
+            onFocusChange={onFocusChange}
+            id='2'
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+        </Form.Group>
 
-      <Form.Group controlId='exampleForm.ControlSelect1'>
-        <FormLabel>Select reader</FormLabel>
-        <Form.Control as='select'>{readersSelectItems}</Form.Control>
-      </Form.Group>
-      <Button onClick={handleSubmit}>Create Challenge</Button>
-    </Form>
+        <Button onClick={handleSubmit}>Create Challenge</Button>
+      </Form>
+    </div>
   )
 }
 
