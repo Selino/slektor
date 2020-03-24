866b319d1a90a1e243ab592c4d9c334a70d86ddc
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-03-24 13:32:15 -0700

    [ch 143] Adding filters reducer tests

---
+++ src/tests/reducers/filtersreducer.test.js
@@ -0,0 +1,46 @@
+import filtersReducer from "../../reducers/FiltersReducer"
+import moment from "moment"
+
+test("should setup default filter values", () => {
+  const state = filtersReducer(undefined, { type: "@@INIT" })
+  expect(state).toEqual({
+    sortBy: "date",
+    startDate: moment().startOf("month"),
+    endDate: moment().endOf("month"),
+    text: ""
+  })
+})
+
+test("should set text to string", () => {
+  const state = filtersReducer(undefined, {
+    type: "SET_TEXT_FILTER",
+    text: "bubba"
+  })
+  expect(state.text).toBe("bubba")
+})
+
+test("should set sort by to amount", () => {
+  const state = filtersReducer(undefined, { type: "SORT_BY_AMOUNT" })
+  expect(state.sortBy).toBe("amount")
+})
+
+test("should set sort by to date", () => {
+  const state = filtersReducer(undefined, { type: "SORT_BY_DATE" })
+  expect(state.sortBy).toBe("date")
+})
+
+test("should set starDate", () => {
+  const state = filtersReducer(undefined, {
+    type: "SET_START_DATE",
+    startDate: moment(0)
+  })
+  expect(state.startDate).toEqual(moment(0))
+})
+
+test("should set endDate", () => {
+  const state = filtersReducer(undefined, {
+    type: "SET_END_DATE",
+    endDate: moment(0)
+  })
+  expect(state.endDate).toEqual(moment(0))
+})
