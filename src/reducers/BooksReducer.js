9f254e8af1532068117023a87834da27b0d1e254
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-03-24 14:04:51 -0700

    [ch 143 ] Adding Books reducer tests

--- src/reducers/BooksReducer.js
+++ src/reducers/BooksReducer.js
@@ -4,7 +4,7 @@ const booksReducerDefault = getSampleData().items
 export default (state = booksReducerDefault, action) => {
   switch (action.type) {
     case "SET_SEARCH_STRING":
-      return { searchString: action.text }
+      return { searchString: action.searchString }
     default:
       return state
   }
---
+++ src/tests/reducers/booksreducer.test.js
@@ -0,0 +1,18 @@
+import booksReducer from "../../reducers/BooksReducer"
+
+test("should setup default items", () => {
+  const state = booksReducer(undefined, { type: "@@INIT" })
+  expect(state).toHaveLength(5)
+  expect(state[0]).toHaveProperty("id", "pD6arNyKyi8C")
+  expect(state[4]).toHaveProperty("id", "N_0VhzQKIIAC")
+})
+
+test("should set search string", () => {
+  const state = booksReducer(undefined, {
+    type: "SET_SEARCH_STRING",
+    searchString: "bubba"
+  })
+  expect(state).toMatchObject({
+    searchString: "bubba"
+  })
+})
