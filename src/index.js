33204c2f944f3ec0fcca63a7f961676021f52f0d
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-03-25 19:56:27 -0700

    [ch171 finish] Cleaning up files. Moved 404 and Help pages into folders with test cases.

--- src/index.js
+++ src/index.js
@@ -5,33 +5,10 @@ import * as serviceWorker from "./serviceWorker"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Provider } from "react-redux"
 import configureStore from "./store/configureStore"
-// import { setTextFilter } from "./actions/filtersactions"
-import { createChallenge } from "./actions/challengesactions"
-import { v4 as uuidv4 } from "uuid"
 import "moment-timezone"
 import AppRouter from "./routers/AppRouter"
 
 const store = configureStore()
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter("poo"))
-// }, 3000)
-
-store.dispatch(
-  createChallenge({
-    id: "4",
-    owner: "Lenny Kravitz",
-    reader: "Butt McJones",
-    status: "running",
-    bookID: uuidv4(),
-    bookTitle: "The Road",
-    bookThumbnail:
-      "http://books.google.com/books/content?id=H8ON-dTgQQYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-    amount: 200,
-    startDate: 1584470612000,
-    endDate: 1585593812000
-  })
-)
 
 ReactDOM.render(
   <Provider store={store}>
