d92cf6191de5ad17240567693fc4c211bb0595bf
Selino Valdes <selinov@gmail.com>
2020-04-09 14:24:04 -0700

    [ch255] Installed an FPO mark into the nav.

--- src/components/nav/GlobalNav.js
+++ src/components/nav/GlobalNav.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { Navbar } from "react-bootstrap"
 import logo from "../../logo.svg"
+import mark from "../../slektor-mark.svg"
 import "./GlobalNav.sass"
 
 export default function globalNav() {
   return (
-    <Navbar className='GlobalNav' fixed='top' bg='dark' variant='dark'>
+    <Navbar
+      className='GlobalNav'
+      fixed='top'
+      bg='light'
+      variant='light'
+      expand='lg'
+    >
       <Navbar.Brand>
         <img
-          alt='Slektor App Logo'
+          alt={process.env.REACT_APP_TITLE}
           src={logo}
           className='d-inline-block App-logo'
         />
-        {process.env.REACT_APP_TITLE}
+        <img
+          alt={process.env.REACT_APP_TITLE}
+          src={mark}
+          className='d-inline-block'
+        />
       </Navbar.Brand>
     </Navbar>
   )
